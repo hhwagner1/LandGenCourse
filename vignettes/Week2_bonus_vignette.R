87c45dc90030a654e3faa4b658a0ad23d64a05c5
Helene Wagner <hhwagner1@gmail.com>
2017-11-12 23:27:12 -0500

    Added Week 3 & 4 vignettes

---
+++ vignettes/Week2_bonus_vignette.R
@@ -0,0 +1,61 @@
+## ----message=FALSE, warning=TRUE-----------------------------------------
+require(sp)
+require(sf)
+require(raster)
+require(GeNetIt)
+require(rasterVis)
+require(tmaptools) 
+require(here)
+
+## ------------------------------------------------------------------------
+data(ralu.site)
+Sites.sf <- st_as_sf(ralu.site)
+class(Sites.sf)
+
+## ---- fig.width=7, fig.height=6------------------------------------------
+par(mar=c(2,2,2,2))
+plot(Sites.sf, pch=16)
+
+## ----fig.show='hold'-----------------------------------------------------
+plot(Sites.sf[,c("Drainage", "Basin")], pch=16)
+plot(Sites.sf[,c("AREA_m2", "Depth_m")], pch=16)
+
+## ------------------------------------------------------------------------
+#if(!dir.exists(paste0(here(),"/output"))) dir.create(paste0(here(),"/output"))
+#st_write(Sites.sf, paste0(here(),"/output/Sites.shp"), delete_dsn = TRUE)
+
+## ------------------------------------------------------------------------
+data(rasters)
+NLCD <- raster(rasters[6])
+NLCD
+
+## ------------------------------------------------------------------------
+NLCD <- ratify(NLCD)
+rat <- levels(NLCD)[[1]]
+rat
+
+## ------------------------------------------------------------------------
+ColTab <- read.csv(system.file("extdata", "Colortable_LULC.csv", 
+                            package = "LandGenCourse"), header=TRUE)
+ColTab$color <- as.character(ColTab$color)
+ColTab$attribute <- as.character(ColTab$attribute)
+ColTab
+
+## ------------------------------------------------------------------------
+rat <- merge(rat, ColTab, by.x="ID", by.y="value", all=FALSE, sort=TRUE)
+levels(NLCD) <- rat
+NLCD@data@attributes
+
+## ----fig.width=7---------------------------------------------------------
+myOrder <- order(NLCD@data@attributes[[1]]$attribute)
+Map <- levelplot(NLCD, att='attribute', colorkey=list(height=0.5), 
+          col.regions=NLCD@data@attributes[[1]]$color[myOrder]) 
+Map
+
+## ----fig.width=7---------------------------------------------------------
+
+Sites.sp <- ralu.site
+
+Map + layer(sp.points(Sites.sp, pch=16, col="yellow", cex=1.1)) +
+  layer(sp.points(Sites.sp, pch=1, col="black", cex=1.1))
+
---
+++ vignettes/Week4_bonus_vignette.R
@@ -0,0 +1,9 @@
+## ----message=FALSE, warning=TRUE-----------------------------------------
+require(here)
+require(hierfstat)
+require(QstFstComp)
+require(EcoGenetics)
+source(system.file("extdata", "supplemental_R_functions.R", 
+                            package = "LandGenCourse"))
+
+
