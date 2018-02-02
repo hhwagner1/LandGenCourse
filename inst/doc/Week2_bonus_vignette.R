19d803782bf48992eefe8a028912ce255bf33c15
Helene Wagner <hhwagner1@gmail.com>
2018-02-01 21:17:39 -0500

    Fixed Week2 Bonus Material (map)

--- inst/doc/Week2_bonus_vignette.R
+++ inst/doc/Week2_bonus_vignette.R
@@ -47,18 +47,17 @@ levels(NLCD) <- rat
 NLCD@data@attributes
 
 ## ----fig.width=7---------------------------------------------------------
-myOrder <- order(NLCD@data@attributes[[1]]$attribute)
 Map <- levelplot(NLCD, att='attribute', colorkey=list(height=0.5), 
-          col.regions=NLCD@data@attributes[[1]]$color[myOrder]) 
+          col.regions=NLCD@data@attributes[[1]]$color) 
 Map
 
 ## ----fig.width=7---------------------------------------------------------
-
 Sites.sp <- ralu.site
 
 Map + layer(sp.points(Sites.sp, pch=16, col="yellow", cex=1.1)) +
   layer(sp.points(Sites.sp, pch=1, col="black", cex=1.1))
 
 ## ----message=FALSE, warning=TRUE, include=FALSE--------------------------
+# Detach all packages except for some basic ones:
 LandGenCourse::detachAllPackages()
 
