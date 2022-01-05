b9758274b0cf08dfc99822e12172795044745ca3
hhwagner1 <hhwagner1@gmail.com>
2022-01-05 11:25:50 -0500

    BUilt vignettes

--- vignettes/Week2_vignette.R
+++
@@ -1,3 +0,0 @@
-## -----------------------------------------------------------------------------
-if(!requireNamespace("GeNetIt", quietly = TRUE)) remotes::install_github("jeffreyevans/GeNetIt")
-
--- vignettes/Week6_vignette.R
+++
@@ -1,5 +0,0 @@
-## -----------------------------------------------------------------------------
-if(!requireNamespace("EcoGenetics", quietly = TRUE)) 
-  remotes::install_github("leandroroser/EcoGenetics-devel")
-if(!requireNamespace("QstFstComp", quietly = TRUE)) remotes::install_github("kjgilbert/QstFstComp")
-
