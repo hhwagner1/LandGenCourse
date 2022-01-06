637be08004db21cf1a4d65dedb6e7bf1efac2dc6
hhwagner1 <hhwagner1@gmail.com>
2022-01-06 14:52:24 -0500

    Manual page for installDGS function

--- R/installDGS.R
+++ R/installDGS.R
@@ -11,7 +11,8 @@ info <- rstudioapi::versionInfo()
 
 # check what version of RStudio is in use
 if (info$version < "1.4") {
-  cat("Please update RStudio. Your version is: ", info$version)
+  cat("Please update RStudio. Your version is: ",
+      as.character(info$version), "\n")
 }
 
 #####################################
@@ -24,6 +25,7 @@ if(tmp == TRUE)
 {
   installr::updateR(fast=TRUE)
 }
+cat("\n")
 
 
 
@@ -46,69 +48,69 @@ if( Sys.info()['sysname'] != "Windows") {
   lib <- file.path(R.home(), "library")
 }
 
-pkg <- as.data.frame(installed.packages())$Package
-  if(!"devtools" %in% pkg) install.packages("devtools")
-    if(!"remotes" %in% pkg) install.packages("remotes")
+pkg <- as.data.frame(utils::installed.packages())$Package
+  if(!"devtools" %in% pkg) utils::install.packages("devtools")
+    if(!"remotes" %in% pkg) utils::install.packages("remotes")
 
 if( Sys.info()['sysname'] == "macOS") {
   if(!"rgeos" %in% pkg) {
-    install.packages("rgeos", repos="http://R-Forge.R-project.org", type="source")}
+    utils::install.packages("rgeos", repos="http://R-Forge.R-project.org", type="source")}
   if(!"rgdal" %in% pkg) {
-    install.packages("rgdal", repos="http://R-Forge.R-project.org", type="source")}
+    utils::install.packages("rgdal", repos="http://R-Forge.R-project.org", type="source")}
 }
 
 #####################################
 # Install CRAN packages from source
 #####################################
 #if(!"popgraph" %in% pkg) {
-#  install.packages("http://cran.r-project.org/src/contrib/Archive/popgraph/popgraph_1.4.tar.gz",
+#  utils::install.packages("http://cran.r-project.org/src/contrib/Archive/popgraph/popgraph_1.4.tar.gz",
 #                   repos=NULL, type="source") }
 #if(!"gstudio" %in% pkg) {
-#  install.packages("http://cran.r-project.org/src/contrib/Archivess/gstudio/gstudio_1.3.tar.gz",
+#  utils::install.packages("http://cran.r-project.org/src/contrib/Archivess/gstudio/gstudio_1.3.tar.gz",
 #                   repos=NULL, type="source") }
 if(!"base64enc" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/base64enc_0.1-3.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/base64enc_0.1-3.tar.gz",
                    repos=NULL, type="source")}
 if(!"cowplot" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/cowplot_1.0.0.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/cowplot_1.0.0.tar.gz",
                    repos=NULL, type="source")}
 if(!"effsize" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/effsize_0.7.6.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/effsize_0.7.6.tar.gz",
                    repos=NULL, type="source")}
 if(!"hierfstat" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/hierfstat_0.04-22.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/hierfstat_0.04-22.tar.gz",
                    repos=NULL, type="source")}
 if(!"mapplots" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/mapplots_1.5.1.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/mapplots_1.5.1.tar.gz",
                    repos=NULL, type="source")}
 if(!"microbenchmark" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/microbenchmark_1.4-7.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/microbenchmark_1.4-7.tar.gz",
                    repos=NULL, type="source")}
 if(!"predictmeans" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/predictmeans_1.0.1.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/predictmeans_1.0.1.tar.gz",
                    repos=NULL, type="source")}
 if(!"profvis" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/profvis_0.3.6.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/profvis_0.3.6.tar.gz",
                    repos=NULL, type="source")}
 if(!"pwr" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/pwr_1.2-2.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/pwr_1.2-2.tar.gz",
                    repos=NULL, type="source")}
 if(!"Sunder" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/Sunder_0.0.4.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/Sunder_0.0.4.tar.gz",
                    repos=NULL, type="source")}
 if(!"usdm" %in% pkg) {
-  install.packages("http://cran.r-project.org/src/contrib/usdm_1.1-18.tar.gz",
+  utils::install.packages("http://cran.r-project.org/src/contrib/usdm_1.1-18.tar.gz",
                    repos=NULL, type="source")}
 
 
 # Install poppr from binary
 #if( Sys.info()['sysname'] == "Windows") {
 #  if(!"poppr" %in% pkg) {
-#    install.packages("http://cran.r-project.org/bin/windows/contrib/4.0/poppr_2.8.3.zip",
+#    utils::install.packages("http://cran.r-project.org/bin/windows/contrib/4.0/poppr_2.8.3.zip",
 #                     repos=NULL, type="win.binary")}
 #} else {
 #  if(!"poppr" %in% pkg) {
-#    install.packages("http://cran.r-project.org/macosx/el-capitan/contrib/3.6/poppr_2.8.3.tgz",
+#    utils::install.packages("http://cran.r-project.org/macosx/el-capitan/contrib/3.6/poppr_2.8.3.tgz",
 #                     repos=NULL, type="mac.binary.el-capitan")}
 #}
 
@@ -118,14 +120,14 @@ if(!"usdm" %in% pkg) {
 #####################################
 # Add require packages function
 libs <- function(x, add = FALSE, install = TRUE) {
-  pkg <- as.data.frame(installed.packages())
+  pkg <- as.data.frame(utils::installed.packages())
   for(i in 1:length(x)) {
     if(!x[i] %in% pkg$Package)
 	{
       cat(x[i], " is not installed","\n\n")
       if(install) {
         cat("    Installing", x[i], "to", file.path(R.home(), "library"), "\n\n")
-	      try( install.packages(x[i], repos = "http://cran.us.r-project.org") )
+	      try( utils::install.packages(x[i], repos = "http://cran.us.r-project.org") )
 		if(add) require(x[i], quietly = TRUE, character.only = TRUE)
       }
 	} else {
@@ -134,7 +136,7 @@ libs <- function(x, add = FALSE, install = TRUE) {
     }
   }
 }
-cran.pkg <- c("ade4", "adegenet", "BiocManager", "car", "compiler", "data.table",
+cran.pkg <- c("ade4", "adegenet", "BiocManager", "car", "compiler", "data.table", "deldir",
               "doParallel", "dplyr", "feather", "fields", "formatR", "gdistance",
               "GeNetIt", "geosphere", "ggeffects", "ggmap", "ggplot2", "gridExtra", "here",
               "httpuv", "igraph", "knitr", "landscapemetrics", "lattice", "lfmm", "lme4", "maps",
@@ -165,7 +167,7 @@ r = "http://cran.r-project.org"
 #  remotes::install_version("formatR", version = "1.5", repos = r, upgrade = "never")}
 #if(!"miniUI" %in% pkg) {
 #  remotes::install_version("miniUI", version = "0.1.1", repos = r, upgrade = "never")}
-if(!"secr" %in% pkg) { install.packages("secr", dependencies="Imports", type="binary")}
+if(!"secr" %in% pkg) { utils::install.packages("secr", dependencies="Imports", type="binary")}
 
 #### Install GitHub packages
 if(!"EcoGenetics" %in% pkg) {
@@ -175,7 +177,7 @@ if(!"EcoGenetics" %in% pkg) {
 if(!"QstFstComp" %in% pkg) {
   remotes::install_github("kjgilbert/QstFstComp", force=FALSE, upgrade = "never")}
 if(!"popgraph" %in% pkg) {
-  install.packages( c("RgoogleMaps", "geosphere", "proto", "sampling", "seqinr", "spacetime","spdep"), dependencies=TRUE )
+  utils::install.packages( c("RgoogleMaps", "geosphere", "proto", "sampling", "seqinr", "spacetime","spdep"), dependencies=TRUE )
   remotes::install_github("dyerlab/popgraph", force=FALSE, upgrade = "never")}
 if(!"gstudio" %in% pkg) {
   remotes::install_github("dyerlab/gstudio", force=FALSE, upgrade = "never")}
@@ -199,7 +201,7 @@ cran.pkg <- unique(c(cran.pkg, "EcoGenetics", "QstFstComp", "popgraph", "gstudio
 			  "microbenchmark", "predictmeans", "profvis", "pwr", "Sunder", "usdm"))
 
 # Check installs and then install LandGenCourse
-pkg <- as.character(as.data.frame(installed.packages())$Package)
+pkg <- as.character(as.data.frame(utils::installed.packages())$Package)
   pkg.idx <- pkg[which(pkg %in% cran.pkg)]
   if(length(pkg.idx) != length(cran.pkg)) {
     cat("The following packages are missing: ", cran.pkg[which(!cran.pkg %in% pkg.idx)], "\n\n")
